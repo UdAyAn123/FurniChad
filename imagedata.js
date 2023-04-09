6fad8e4dfe1c1c4867502d226ce722aeb2ecbdd0
Dev-wizzychat <102896441+Dev-wizzychat@users.noreply.github.com>
2023-04-09 11:33:11 +0530

    Add files via upload

--- imagedata.js
+++ imagedata.js
@@ -184,11 +184,8 @@ function copy2() {
     var copyText = document.getElementById("design");
 
     // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
 
     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
 
     // Alert the copied text
 
